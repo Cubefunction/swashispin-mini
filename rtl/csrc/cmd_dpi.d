647aa1ce18594e9c1f641678123b871eefa13acd
ShizeChe <shizeche@seas.upenn.edu>
2026-01-22 21:15:43 -0500

    delete simulator build artifacts

--- rtl/csrc/cmd_dpi.d
+++
@@ -1,2 +0,0 @@
-cmd_dpi.o: ../sim/cmd_dpi.c \
- /synopsys/tools/vcs/V-2023.12-1/include/svdpi.h
--- rtl/csrc/import_dpic.h
+++
@@ -1,6 +0,0 @@
-
- extern int cmd_open(/* INPUT */const char* path);
-
- extern int cmd_accept_poll(/* INPUT */int timeout_ms);
-
- extern int cmd_getline(const /* OUTPUT */svOpenArrayHandle line_buf);
