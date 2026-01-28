49c70655ecb4c877c8dd1159493f1fd29557ab6d
Cubefunction <linhaid@seas.upenn.edu>
2026-01-27 22:14:16 -0500

    adcop

--- asm/include/adc.h
+++ asm/include/adc.h
@@ -3,6 +3,11 @@
 
 #include <stdint.h>
 
+#define ADC_OP_BASE        0xADCA0000u
+
+#define ADC_OP_DELAY_NS    (ADC_OP_BASE | 0x0001)
+#define ADC_OP_SAMPLE_NS   (ADC_OP_BASE | 0x0002)
+
 typedef struct {
     uint32_t delay_ns;
     uint32_t sample_ns;
